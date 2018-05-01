218c30c587923c2419a71f5c25723488265dad06
EC2 Default User <ec2-user@ip-172-31-20-243.us-east-2.compute.internal>
2018-05-01 11:16:34 +0000

    fifthweek

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+    end
+    
+    def create
+        params.permit!
+        
+        @article = Article.new(params[:article])
+        @article.save
+        
+        redirect_to @article
+    end
+    
+    def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        
+        redirect_to articles_path
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        params.permit!
+        
+        @article = Article.find(params[:id])
+        @article.update(params[:article])
+        
+        redirect_to @article
+    end
+end
